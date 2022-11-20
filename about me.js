23218d2f227883cb5437862887c97ca03597f28a
1olo6 <118646695+1olo6@users.noreply.github.com>
2022-11-20 06:57:24 +0300

    Add files via upload

---
+++ about me.js
@@ -0,0 +1,8 @@
+
+        function imgss()
+        {
+            var x=document.getElementById('img2');
+            if(x.src.match('mb.png'))
+            {x.src="mb2.jpg";}
+            else{x.src="mb.png";}
+        }
---
+++ mypro.js
@@ -0,0 +1,1 @@
+window.alert("Welcom to Box App")
